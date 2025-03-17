8506a7aa65bd84dde2808c4fe214d6bb4899246b
SPLIT-TORRES7 <split.torr3s7@gmail.com>
2025-03-16 19:33:56 -0600

    Update examen.py

--- examen.py
+++ examen.py
@@ -1,12 +1,9 @@
 cont_corr = "Gustambo university"
 while True:
-    for i in range(5):
+    for i in:
         cont_usu = input(f"Dame la contraseña") 
         if cont_corr == cont_usu: 
             print("Contraseña correcta")
-            break
-        print(f"Contraseña incorrecta, intento{i+1}")
     else:
         print("Contraseña incorrecta")
     
-    break 
